e3b4157426466da2a0e05b9ce7069e867b6166e0
souzayasmin <159201472+souzayasmin@users.noreply.github.com>
2024-03-25 15:34:40 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+import time
+
+número1 = 10 
+número2 = 15
+
+print("Adição:", número1 + número2)
+print("Subtração:", número1 - número2)
+print("Multiplicação:", número1 * número2)
+print("Divisão:", número1 / número2)
+print("\n\nDivisão inteira:", número1 // número2)
+print("\n\nResto da divisão:", número1 % número2)
+print("\n\npotência:", número1 ** número2)
+
+time.sleep(5)
